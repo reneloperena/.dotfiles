c0b93427146ba6cf2be6b191fc49b09f7092792e
Rene Loperena <reneloperena@live.com>
2017-10-30 20:19:45 -0400

    Fix tunes.js

--- tmux/tunes.js
+++ tmux/tunes.js
@@ -1,22 +1,22 @@
-let output = '';
+/* global Application */
+let output = ''
 if (Application('iTunes').running()) {
-    const track = Application('iTunes').currentTrack;
-    const artist = track.artist();
-    const title = track.name();
-    output = (${title} - ${artist})
+  const track = Application('iTunes').currentTrack
+  const artist = track.artist()
+  const title = track.name()
+  output = (`${title} - ${artist}`)
 } else if (Application('Spotify').running()) {
-    const track = Application('Spotify').currentTrack;
-    const artist = track.artist();
-    const title = track.name();
-    output = (${title} - ${artist})
+  const track = Application('Spotify').currentTrack
+  const artist = track.artist()
+  const title = track.name()
+  output = (`${title} - ${artist}`)
 }
-size = Math.ceil(output.length / 50)
-mod = (new Date()).getSeconds() % size
-
+const size = Math.ceil(output.length / 50)
+const mod = (new Date()).getSeconds() % size
 
 output = output.substr(50 * mod, (50 * mod) + 50)
 
-missingPad = 50 - output.length
+const missingPad = 50 - output.length
 
 for (let i = 0; i < missingPad; i++) {
   output += ' '
